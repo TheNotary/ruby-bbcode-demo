7390a236f3a302a0751f5f62e35c448c16856cd8
TheNotary <no@mail.plz>
2015-02-11 07:27:41 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyBbcodeDemo::Application.config.secret_key_base = '6f12499642fbb2fdf6f6c46d29a68e9430bfe15bfa8c5b79228f4195d0c9956fa9009a64b5b9770a3682095c2e8c3f6f7b441aa4ba42b665ad768bdfa1affdcd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyBbcodeDemo::Application.config.session_store :cookie_store, key: '_ruby-bbcode-demo_session'
